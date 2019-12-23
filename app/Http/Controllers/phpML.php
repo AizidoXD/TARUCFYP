90407f96ad94e5e34ff03c131038ad8a6ca37ff0
Woo Guo Hau <Woo Guo Hau@LAPTOP-IO9U9PB1>
2019-12-23 20:54:10 +0800

    Update for reading csv

--- app/Http/Controllers/phpML.php
+++ app/Http/Controllers/phpML.php
@@ -16,18 +16,22 @@ use Illuminate\Http\Request;
 class phpML extends Controller {
 
     //initialize 2 array for training ( text and label ) 
-    private $arr_text = ["Bus is old, bumpy.", "We couldn't find a place to sit.", "The bus is bad.", "Clean bus. Great driver.", "Excellent driver!", "Driver was professional and on time ! Would use them again.", "The bus driver is professional"];
-    private $arr_label = ["BusB", "BusB", "BusB", "BusG", "BusG", "BusG", "BusG"];
+    //private $arr_text = ["Bus is old, bumpy.", "We couldn't find a place to sit.", "The bus is bad.", "Clean bus. Great driver.", "Excellent driver!", "Driver was professional and on time ! Would use them again.", "The bus driver is professional"];
+    //private $arr_label = ["BusB", "BusB", "BusB", "BusG", "BusG", "BusG", "BusG"];
+
+    private $arr_text = [];
+    private $arr_label = [];
 
     public function index() {
         // extract data from csv file and put into arr_text and arr_label
-//        if (($handle = fopen("test.csv", "r")) !== FALSE) {
-//            while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
-//                array_push($this->arr_text, strtolower($data[0]));
-//                array_push($this->arr_label, $data[1]);
-//            }
-//            fclose($handle);
-//        }
+        //$filePath = public_path('/Excel/')."Bus.csv";
+        if (($csvFile = fopen("Excel/Bus.csv", "r")) !== FALSE) {
+            while (($data = fgetcsv($csvFile, 1000, ",")) !== FALSE) {
+                array_push($this->arr_text, strtolower($data[0]));
+                array_push($this->arr_label, $data[1]);
+            }
+            fclose($csvFile);
+        }
         
         // tokenize the sentence
         $tokenize = new WordTokenizer();
@@ -55,10 +59,11 @@ class phpML extends Controller {
         // initialize test set 
         $arr_testset = [
             'Great driver',
-            'professional driver',
+            'worst driver',
             'The driver professional',
-            'clean bus',
-            'The bus is professional'
+            'dirty bus',
+            'The bus is professional',
+            'I woul highly recomend this bus'
         ];
 
         $vectorizer->transform($arr_testset);
@@ -68,7 +73,7 @@ class phpML extends Controller {
 //        foreach ($arr_testset as $a) {
 //            echo $a . '</br>';
 //        }
-        
+
         foreach ($result as $value) {
             echo $value . '</br>';
         }
