35de7203ab2f10de4754fee833b0e58ff458f0c3
Woo Guo Hau <Woo Guo Hau@LAPTOP-IO9U9PB1>
2019-12-25 14:51:07 +0800

    report update with dynamic result

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -1,23 +1,38 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
+<div id="chart_div" style="width: 100%; height: 700px;">
+<script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+    <script type="text/javascript">
+      google.charts.load('current', {'packages':['corechart']});
+      google.charts.setOnLoadCallback(drawVisualization);
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+      function drawVisualization() {
+        // Some raw data (not necessarily accurate)
+        var data = google.visualization.arrayToDataTable([
+          ['Category', 'Bad', 'Good'],
+          ['Bus',  {{$arr_bus[0]}},      {{$arr_bus[1]}}],
+        ]);
 
-                    
-                </div>
-            </div>
-        </div>
-    </div>
+        var options = {
+          title : 'TARUC Sentiment Analysis on xx/xx/xxxx',
+          vAxis: {title: 'Total Comments'},
+          hAxis: {title: 'Category'},
+          seriesType: 'bars',
+          series: {2: {type: 'line'}},    
+		  colors: ['#80FF00','FF0000']
+		  };
+			
+        var chart = new google.visualization.ComboChart(document.getElementById('chart_div'));
+        chart.draw(data, options);
+      }
+    </script>
+</div>
+
+<div style="width: 500px; height: 500px; border: 1px black solid">
+    <h5>Issue Ranking</h5>
+    Bus - 1</br>
+    Wifi - 2</br>
+    Parking - 3</br>
 </div>
 @endsection
