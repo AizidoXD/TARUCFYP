06396572a08bc805fe9ec7baba6488fd3dac284b
Woo Guo Hau <Woo Guo Hau@LAPTOP-IO9U9PB1>
2019-12-26 20:43:44 +0800

    Multiple result updated

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -10,8 +10,34 @@
       function drawVisualization() {
         // Some raw data (not necessarily accurate)
         var data = google.visualization.arrayToDataTable([
-          ['Category', 'Bad', 'Good'],
-          ['Bus',  {{$arr_bus[0]}},      {{$arr_bus[1]}}],
+          ['Category', 'Good', 'Bad'],
+          @if($arr_bus[2] > 0)
+          ['Bus', {{$arr_bus[0]}}, {{$arr_bus[1]}}],
+          @endif
+          @if($arr_wifi[2] > 0)
+          ['Wifi', {{$arr_wifi[0]}}, {{$arr_wifi[1]}}],
+          @endif
+          @if($arr_adminService[2] > 0)
+          ['Admin Services', {{$arr_adminService[0]}}, {{$arr_adminService[1]}}],
+          @endif
+          @if($arr_food[2] > 0)
+          ['Food', {{$arr_food[0]}}, {{$arr_food[1]}}],
+          @endif
+          @if($arr_parking[2] > 0)
+          ['Parking', {{$arr_parking[0]}}, {{$arr_parking[1]}}],
+          @endif
+          @if($arr_timeTable[2] > 0)
+          ['Time Table', {{$arr_timeTable[0]}}, {{$arr_timeTable[1]}}],
+          @endif
+          @if($arr_lecturer[2] > 0)
+          ['Lecturer', {{$arr_lecturer[0]}}, {{$arr_lecturer[1]}}],
+          @endif
+          @if($arr_outdoor[2] > 0)
+          ['Outdoor', {{$arr_outdoor[0]}}, {{$arr_outdoor[1]}}],
+          @endif
+          @if($arr_facility[2] > 0)
+          ['Facility', {{$arr_facility[0]}}, {{$arr_facility[1]}}],
+          @endif
         ]);
 
         var options = {
