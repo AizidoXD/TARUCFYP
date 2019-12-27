9ae9c3b695b95e320316d1bb4f917aa3aa843eed
Woo Guo Hau <Woo Guo Hau@LAPTOP-IO9U9PB1>
2019-12-28 00:59:35 +0800

    home view updated

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,14 +9,59 @@
 
                 <div class="card-body">
                     @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
+                    <div class="alert alert-success" role="alert">
+                        {{ session('status') }}
+                    </div>
                     @endif
 
                     <a href="{{ url('upload_excel') }}">Import Excel File.</a></br>
                     <a href="{{ url('test_ml') }}">Run Analysis</a> This should show the list of csv file
                 </div>
+            </div>
+
+            <br>
+
+            <div class="card">
+                <form action="{{route('analyzing')}}" method="post">
+                    <div class="card-header">CSV File List</div>
+                    <div class="card-body">
+                        <?php
+                        $dir = public_path('/Excel/Test/');
+                        $csvList = scandir($dir, 1);
+                        ?>
+                        <table style="width: 100%">
+                            <tr>
+                                <th>File Name</th>
+                                <th>Select Here</th>
+                            </tr>
+                            @for($i = 0; $i < count($csvList) - 2; $i++)
+                            <tr>
+                                <td>
+                                    {{$csvList[$i]}}
+                                </td>
+                                <td>
+                                    @if($i === 0)
+                                    <input type="radio" id="{{$i}}" name="csvName" value="{{$csvList[$i]}}" checked>
+                                    @else
+                                    <input type="radio" id="{{$i}}" name="csvName" value="{{$csvList[$i]}}">
+                                    @endif
+                                </td>
+                            </tr>
+                            @endfor
+                        </table>
+                        <input type="checkbox" name="Bus" value="Bus" checked required>Bus &nbsp;
+                        <input type="checkbox" name="Wifi" value="Wifi">Wifi &nbsp;
+                        <input type="checkbox" name="AdminService" value="AdminService">AdminService &nbsp;
+                        <input type="checkbox" name="Food" value="Food">Food &nbsp;
+                        <input type="checkbox" name="Parking" value="Parking">Parking &nbsp;
+                        <input type="checkbox" name="TimeTable" value="TimeTable">TimeTable &nbsp;
+                        <input type="checkbox" name="Lecturer" value="Lecturer">Lecturer &nbsp;
+                        <input type="checkbox" name="Outdoor" value="Outdoor">Outdoor &nbsp;
+                        <input type="checkbox" name="Facility" value="Facility">Facility
+                        <input type="submit" value="Analyze" style="float: right"/>
+                        {{csrf_field()}}
+                    </div>
+                </form>
             </div>
         </div>
     </div>
