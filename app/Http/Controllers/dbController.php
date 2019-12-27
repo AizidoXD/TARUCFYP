3d5aa249237cd5b248f2a86dd2028dbff9f9dc5e
Woo Guo Hau <Woo Guo Hau@LAPTOP-IO9U9PB1>
2019-12-27 23:24:39 +0800

    split result string to dbCOntroller

--- app/Http/Controllers/dbController.php
+++ app/Http/Controllers/dbController.php
@@ -7,10 +7,11 @@ use Illuminate\Http\Request;
 class dbController extends Controller
 {
     public function index(Request $request){
-        dd($request->all());
-//        for($i = 0; $i < $request->input('size'); $i++){
-//            $arr = explode(",",$request->input('0'));
-//            printr($arr);
-//        }
+//        dd($request->all());
+        $size = $request->input('size');
+        for($i = 0; $i < $size; $i++){
+            $arr = explode(",",$request->input($i));
+            printf ("No:" . $i . " " . $arr[0] . " " . $arr[1] . " " . $arr[2] . "\n");
+        }
     }
 }
